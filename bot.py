7dd9cd2c16ef8f4a2c6ef7aed8811f2e6ccba62d
Howtog41 <howtog41@gmail.com>
2024-11-18 16:05:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ API_HASH = 'bb32e00647b1bfe66e6cd298a2c66a5a'  # Replace with your API Hash
 BOT_TOKEN = '5645711998:AAE8oAHzKi07iqcydKPnuFjzknlVa2MxxUQ'  # Replace with your bot token
 
 # Source channel information
-SOURCE_CHANNEL = "-1001984768732"  # Replace with source channel username or ID
+SOURCE_CHANNEL = "@LKD_Latest_Korean_Dramaa"  # Replace with source channel username or ID
 
 # Create Pyrogram Client
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
